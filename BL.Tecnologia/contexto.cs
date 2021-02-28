1fcae7fcf6dc480a03d8e751e30d30ffcb936dcb
moises orellana <moorellana@unah.hn>
2021-02-28 12:45:48 -0600

    Creando Base de Datos

---
+++ BL.Tecnologia/contexto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Tecnologia
+{
+     public class contexto: DbContext
+    {
+        public contexto(): base("electronicos")
+        {
+
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
